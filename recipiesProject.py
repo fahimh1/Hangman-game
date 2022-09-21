6eec08cdf9f2ce493434ca1ad24aa34ea702ee7c
fahimhasan1 <FHasan@spartaglobal.com>
2022-09-21 15:20:08 +0100

    added comments and better formating

--- recipiesProject.py
+++ recipiesProject.py
@@ -10,49 +10,79 @@ my_dir =Path('C:/Users/Fahim/Documents/udemypy/day 6 files and folders/Recipes/R
 
 def choose_category():
     valid_category = [i for i in os.listdir(my_dir)]
+    # it is a list of all the categories that are in the directory
     valid_check = [i.lower() for i in valid_category]
+    # is same list as before but with all entities in lower case to compare to user input
     #print(valid_category)
     user_choice = ''
     while user_choice not in valid_check:
         user_choice = input('By typing the name please choose a category  ' + ', '.join(valid_category) + ' ').lower()
+        # takes the user input
         if user_choice in valid_check:
             category_index = (valid_check.index(user_choice))
+            # compares the input to the available categories in the list a return the index of the item
             file_paths = list((Path(my_dir/valid_check[category_index]).glob('*.txt')))
+            # is a list of all the file paths in the directory
             file_dir = Path(my_dir/valid_check[category_index])
+            #is a file path of the selected category directory
             file_names = [i.name for i in file_paths]
+            # is a list of all the files names in the chosen directory
             #print(file_paths)
             return file_names, file_paths, file_dir
         else:
-            print('Input is not recognised. please try again ')
+            print('Input is not recognised. Please try again ')
 
-# asks the user to choose an option form the provided categories
+# The function above asks the user to choose an option from the provided categories.
+# The user inputs their choice, it is compared to the valid_categories (existing directories).
+# The file path of the directory as well as the names and files paths of the files located in the directory are returned.
 
 
 def choose_recipes(category,file_paths):
     #print(category)
     user_recipe = ''
     valid_recipes = [items.lower().strip('.txt') for items in category]
+    # is a list of recipes that exists in the file path given
     #print(valid_recipes)
     while user_recipe not in valid_recipes:
         user_recipe = input('Enter a recipe from the following recipes -------> ' + ', '.join(items.strip('.txt')
                                                                                              for items in
                                                                                              category) + '.\n').lower()
+        # takes the user input for the recipes and also displays the all recipes in the directory
         if user_recipe in valid_recipes:
+            # checks if the user's input is valid (is and actual file in the directory)
             recipe_index = valid_recipes.index(user_recipe)
+            # gets the correct index from the valid list for the users input
             system("cls")
+            #clears the terminal
             print(file_paths[recipe_index].read_text())
+            # using the index attained the correct file path is obtained from the list
+            # and displays the contents of the chosen txt file
             return file_paths[recipe_index]
         else:
             print('Recipe not found. please try again '.upper())
 
+# The function above takes the users choice of recipe (.txt file).
+# The function takes the argument of category (which is the chosen directory) and file_paths (which is the
+# file path of the chosen directory). It then aks the user to choose a recipe from the displayed options and checks
+# if it is in the directory if not it then asks the user to try again. If the user input is valid it then gets the
+# file path from a list and then displays the contents of the txt file.
 
 
 def new_recipe(file_dir):
     os.chdir(file_dir)
+    # changes the directory to the chosen directory
     recipe_name = input('Enter the name of you new recipe ')
     recipe_content = input('Enter the new recipe ')
+    # asks the user to input the name of th new recipe and the contents of it
     with open(f'{recipe_name}.txt','w' ) as file:
         file.writelines(recipe_content)
+    # writes the user input to the file
+
+# The function above creates a new recipe (.txt file) in the chosen directory
+# The function takes an argument of 'file_dir' which is a file path of the user chosen directory obtained from
+# another function. The function creates a new recipe in a chosen directory.
+# Firstly the directory is changed to the correct one, the user is then asked to input the name of the file and then
+# the content of the file which is then written to the file and the file the is added to the directory.
 
 def new_category():
     all_categories = [i for i in os.listdir(my_dir)]
@@ -61,16 +91,26 @@ def new_category():
     new_user_category = input("Enter the name of the new category ")
     os.makedirs(new_user_category)
 
+# The function above created a new category(directory).
+# It First displays the current categories and asks the user to input a new one, which is then created
+
 def remove_category(file_dir):
     shutil.rmtree(file_dir)
     all_categories = [i for i in os.listdir(my_dir)]
-    print('You have delete the category, the reaming categories are as follows -------> '+','.join(all_categories)+'\n')
+    print('You have delete the category, the remaining categories are as follows -------> '+','.join(all_categories)+'\n')
+
+# The function above removes a category(directory)
+# It displays the categories the exists and asks the user to pick one to delete, all the files in the directory are
+# also deleted.
+
 
 def remove_file(file):
     system("cls")
     print('you have deleted the file ' + file.name)
     os.remove(file)
 
+# This function deletes a recipe(.txt file)
+# The user choose a file from the choose_recipe function, it is passed through to this function and deleted the file
 
 
 
@@ -86,6 +126,18 @@ def remove_file(file):
 
 #remove_file(file)
 
+
+
+
+
+
+#####################################################################################################################
+##################################**************** MAIN **********************#######################################
+#####################################################################################################################
+
+# The first while loop compares the user input to a string if they both match the program is terminated.
+# If not the user has to enter a 1,2,3,4 or 5 for to choose the options displayed
+# The functions are then called to the corresponding user choice
 user = ''
 
 while user.upper() != 'EXIT':
@@ -96,8 +148,9 @@ while user.upper() != 'EXIT':
     if user == str(1) or user == str(2) or user == str(3):
         system('cls')
         category, file_paths, file_dir = choose_category()
-        user = input('Do you want to\n1)Do you want to view a recipe\n2)Create a new recipe\n3)Delete a recipe\n')
-        while user != str(1) or str(2) or str(3) or 'EXIT':
+        user = input('Do you want to\n1)Do you want to view a recipe\n2)Create a new recipe\n3)Delete a recipe\n'
+                     'or type back to go the the main menu\n')
+        while user != str(1) or str(2) or str(3) or 'back':
             if user == str(1):
                 file = choose_recipes(category,file_paths)
                 input('Press any key to return to the main menu ')
@@ -114,11 +167,16 @@ while user.upper() != 'EXIT':
                 input('Press any key to return to the main menu ')
                 system('cls')
                 break
-            elif user.upper() == 'EXIT':
-                exit()
+            elif str(user).lower() == 'back':
+                system('cls')
+                break
             else:
+                system('cls')
+                print(user)
                 user = input(
-                    'Do you want to 1)Do you want to view a recipe\n 2)Create a new recipe\n 3)Delete a recipe')
+                    'Do you want to \n1)Do you want to view a recipe\n 2)Create a new recipe\n 3)Delete a recipe'
+                    'or type back to go the the main menu\n')
+
     elif user == str(4):
         system('cls')
         new_category()
